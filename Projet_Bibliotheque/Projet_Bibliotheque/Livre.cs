7c176fbd21fde0afc6753757cf98492b6cf22fd0
Alex-31 <sio.santoscabrita@gmail.com>
2015-01-08 16:44:28 +0100

    isbn

--- Projet_Bibliotheque/Projet_Bibliotheque/Livre.cs
+++ Projet_Bibliotheque/Projet_Bibliotheque/Livre.cs
@@ -11,22 +11,23 @@ namespace Projet_Bibliotheque
         /// <Propriété>
         private List<string> _typeGenre = new List<string> { "Action", "Aventure", "ScienceFiction", "Fantastique", "Roman", "Policier", "Horreur", "Drame", "BD", "Poeme", "Historique", "Biographie", "Conte", "Fiction" };
         private List<string> _auteur = new List<string>();
-        public int _nb_ISBN { get; set; }
+        
+        public string _nb_ISBN { get; set; }
         
 
         /// <Constructeur par defaut>
         Livre()
         {
             _auteur.Add("Inconnu");
-            _nb_ISBN = 0;
+            _nb_ISBN = "";
             _typeGenre.Add("Inconnu");
         }
 
-        Livre(string auteur, string genre)
+        Livre(string unAuteur, string unGenre, string unNbISBN)
         {
-            _auteur.Add(auteur);
-            _typeGenre.Add(genre);
-            _nb_ISBN = _nb_ISBN + 1;
+            _auteur.Add(unAuteur);
+            _typeGenre.Add(unGenre);
+            _nb_ISBN = unNbISBN; // C'est juste un string à ne pas s'occuper par la suite et non un int
         }
 
     }
