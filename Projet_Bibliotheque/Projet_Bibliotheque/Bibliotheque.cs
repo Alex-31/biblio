6f681f543bedd2af6f966bdae670d756ad4dead0
Alex-31 <sio.santoscabrita@gmail.com>
2015-01-17 12:17:43 +0100

    bdd

--- Projet_Bibliotheque/Projet_Bibliotheque/Bibliotheque.cs
+++ Projet_Bibliotheque/Projet_Bibliotheque/Bibliotheque.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace Projet_Bibliotheque
 {
     class Bibliotheque
-    {
+    { /*
         public Media _genre { get; set; }
 
         Media listerBiblio();
         Media RechercherLivre(string titre, int id, string genre);
         Media RechercherCD(string titre, int id, string styleMusic);
-        Media IncepterElement(int id);
+        Media IncepterElement(int id);*/
 
     }
 }
